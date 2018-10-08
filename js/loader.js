909490b034bb052901f0b80cce27f154b1279cf0
某罐头菠萝 <heglan@mail2.sysu.edu.cn>
2018-10-09 05:14:09 +0800

    add main menu scrolling animation

--- js/loader.js
+++ js/loader.js
@@ -50,3 +50,39 @@ function loadPages() {
         nowLoadingID += 1;
     }
 }
+
+
+// main menu
+if ($(window).width() > 600) {
+    $('body')
+        .visibility({
+            offset: -10,
+            observeChanges: false,
+            once: false,
+            continuous: false,
+            onTopPassed: function () {
+                requestAnimationFrame(function () {
+                    $('.following.bar')
+                        .addClass('light fixed')
+                        .find('.menu')
+                        .removeClass('inverted')
+                        ;
+                    $('.following .additional.item')
+                        .transition('scale in', 750)
+                        ;
+                });
+            },
+            onTopPassedReverse: function () {
+                requestAnimationFrame(function () {
+                    $('.following.bar')
+                        .removeClass('light fixed')
+                        .find('.menu')
+                        .addClass('inverted')
+                        .find('.additional.item')
+                        .transition('hide')
+                        ;
+                });
+            }
+        })
+        ;
+}
