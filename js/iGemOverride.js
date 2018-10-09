b69a192d0d42bfc67f538069be93515224c788d0
某罐头菠萝 <heglan@mail2.sysu.edu.cn>
2018-10-10 02:50:23 +0800

    update override js

--- js/iGemOverride.js
+++ js/iGemOverride.js
@@ -1,7 +1,11 @@
-$('#content').css({
-    "width": "100%",
-    "padding": "0px",
-    "margin": "0px"
-});
-$('#mw-content-text').children('p').remove();
-$('#top_title').remove();
+if (window.location.href.indexOf('2018.igem.org') != -1) {
+    $('#content').css({
+        "width": "100%",
+        "padding": "0px",
+        "margin": "0px"
+    });
+    $('#mw-content-text').children('p').remove();
+    $('#top_title').remove();
+} else {
+    console.warn("Not in igem wiki environment.");
+}
