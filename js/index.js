11934418123c41aa2620d7146c4d85cbe00e7e86
yanbin <yanb25@mail2.sysu.edu.cn>
2018-10-10 22:13:02 +0800

    when click navigator bar, redirect to first eelemnt

--- js/index.js
+++ js/index.js
@@ -26,5 +26,16 @@ $('.dropdown').dropdown({
 $('.ui.dropdown.link.item').on('mouseenter', function () {
   $('.ui.dropdown.link.item').not($(this)).dropdown('hide');
   $(this).dropdown('show');
-});
 
+});
+$('.ui.dropdown.link.item').on('click', function() {
+  // 当标题栏的标题被点击时，默认当作点击dropdown第一篇文章
+  $b = $(this).get(0).children[1].children[0];
+  $b.click();
+})
+;
+
+$('#home').on('mouseenter', function() {
+  console.log('hei');
+  $('.ui.dropdown.link.item').dropdown('hide');
+})
