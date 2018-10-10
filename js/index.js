88b410d2012dc3e01b5e7071f28ba6b22fed91ad
xwy27 <xuwy27@mail2.sysu.edu.cn>
2018-10-11 01:20:30 +0800

    remove dropdown menu click

--- js/index.js
+++ js/index.js
@@ -27,14 +27,7 @@ $('.dropdown').dropdown({
 $('.ui.dropdown.link.item').on('mouseenter', function () {
   $('.ui.dropdown.link.item').not($(this)).dropdown('hide');
   $(this).dropdown('show');
-
 });
-$('.ui.dropdown.link.item').on('click', function() {
-  // 当标题栏的标题被点击时，默认当作点击dropdown第一篇文章
-  $b = $(this).get(0).children[1].children[0];
-  $b.click();
-})
-;
 
 $('#home').on('mouseenter', function() {
   console.log('hei');
