ff71f8643aef001fb6d346ed26e6b6ecb0b0e919
yanbin <yanb25@mail2.sysu.edu.cn>
2018-10-13 23:53:41 +0800

    fix important bug: anchor jump error

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   let dstPage = 0;
   urlSplit = window.location.href.split('#');
   if (urlSplit.length > 1) {
-    dstPage = 1 + pageList.findIndex((value, index, arr) => {
+    dstPage = 0 + pageList.findIndex((value, index, arr) => {
       return value === urlSplit[1];
     });
   }
