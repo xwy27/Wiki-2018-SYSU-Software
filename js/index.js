0a4b9c488b3f7e0f15a119fba984776f0fc62119
某罐头菠萝 <heglan@mail2.sysu.edu.cn>
2018-10-16 01:34:16 +0800

    update side navigator

--- js/index.js
+++ js/index.js
@@ -32,61 +32,7 @@ $(document).ready(function () {
 
 });
 
-let index_map = {
-  1: 0,
-  2: 1,
-  3: 1,
-  4: 1,
-  5: 1,
-  6: 1,
-  7: 1,
-  8: 1,
-  9: 1,
-  10: 1,
-  11: 2,
-  12: 2,
-  13: 2,
-  14: 2,
-  15: 2,
-  16: 3,
-  17: 3,
-  18: 3,
-  19: 3,
-  20: 3,
-  21: 4,
-  22: 4,
-  23: 4,
-  24: 4,
-  25: 5,
-  26: 5,
-  27: 5,
-  28: 5,
-  29: 8,  /* Wet-lab */
-  30: 8,  /* Dry-lab */
-  31: 6,
-  32: 6,
-  33: 6,
-  34: 6,
-  35: 7,
-  36: 7,
-  37: 7,
-  38: 7,
-  39: 7,
-  40: 8,
-  41: 8,
-  42: 8,
-  43: 8
-};
 
-$('.myfkpage').children('li').each(function (index, ele) {
-  let temp = index_map[index];
-  if (temp === 0) {
-    $(this).addClass('noscroll');
-  } else {
-    $(this).addClass('scroll');
-  }
-  $(this).children('.page-container').addClass('test' + temp);
-});
 
 $('.dropdown').dropdown({
   action: 'select'
